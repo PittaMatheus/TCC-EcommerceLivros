f37846646279153aaacd3ba3bc292d8b97c19978
matheus pitta <matheus.pitta12@gmail.com>
2019-06-05 19:30:48 -0300

    Regra de negocio validaCupomDesconto adicionada. Resta redirecionar a mensagem para a pagina de confirmacao de cartao

--- livraria-core/src/livraria/core/controle/Fachada.java
+++ livraria-core/src/livraria/core/controle/Fachada.java
@@ -64,6 +64,7 @@ import livraria.core.regras.livro.ValidarDimensoesLivro;
 import livraria.core.regras.livro.ValidarGrupoPrecificacao;
 import livraria.core.regras.pedido.ConfirmaDados;
 import livraria.core.regras.pedido.ValidaCartaoCredito;
+import livraria.core.regras.pedido.ValidaCupomDesconto;
 import livraria.core.regras.pedido.ValidaEstoque;
 import livraria.core.regras.pedido.ValidaValorPedido;
 
@@ -171,6 +172,7 @@ public class Fachada implements IFachada{
         //Lista de regras de negocio do pedido
         RNPedido.add(new ValidaValorPedido());
         RNPedido.add(new ValidaCartaoCredito());
+        RNPedido.add(new ValidaCupomDesconto());
         
         Map<String, List<IStrategy>> regrasPedido = new HashMap<String, List<IStrategy>>();
         regrasPedido.put("salvar", RNPedido);
---
+++ livraria-core/src/livraria/core/regras/pedido/ValidaCupomDesconto.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package livraria.core.regras.pedido;
+
+import ecommerce.dominio.EntidadeDominio;
+import ecommerce.dominio.pedido.Cupom;
+import ecommerce.dominio.pedido.Pedido;
+import java.util.List;
+import livraria.core.aplicacao.Resultado;
+import livraria.core.dao.pedido.CupomDescontoDAO;
+import livraria.core.regras.AbstractStrategy;
+
+/**
+ *
+ * @author matheus
+ */
+public class ValidaCupomDesconto extends AbstractStrategy{
+
+    @Override
+    public String validar(EntidadeDominio entidade) {
+        String str = "";
+        Pedido pedido = (Pedido) entidade;
+        Cupom cupom = new Cupom();
+        Resultado resultado = new Resultado();
+        CupomDescontoDAO cupomDescDAO = new CupomDescontoDAO();
+        String codigoCupom = pedido.getCupom().getCodigo();
+        cupom.setCodigo(codigoCupom);
+        resultado = cupomDescDAO.consultar(cupom);
+        List<Cupom> cupons = (List) resultado.getEntidades();
+        if(cupons.isEmpty()){
+            str = "<li>Cupom inválido</li>";
+        }
+        
+        return str;
+    }
+    
+}
